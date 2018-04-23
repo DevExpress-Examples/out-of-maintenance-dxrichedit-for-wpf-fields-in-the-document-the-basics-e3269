ca3c548fea915b307708055c76053a4def14071b
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Windows;
+using DevExpress.XtraRichEdit.API.Native;
+
+namespace FieldsExample {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+        private void btnAddField_Click(object sender, RoutedEventArgs e) {
+            Document document = richEditControl1.Document;
+            document.Fields.Add(document.CaretPosition, "DATE");
+        }
+
+        private void btnUpdateField_Click(object sender, RoutedEventArgs e) {
+            if (richEditControl1.Document.Fields.Count > 0)
+                richEditControl1.Document.Fields.Update();
+        }
+
+        private void btnShowFieldCode_Click(object sender, RoutedEventArgs e) {
+            if (richEditControl1.Document.Fields.Count > 0)
+                foreach (Field _f in richEditControl1.Document.Fields){
+                _f.ShowCodes = true;
+                }
+        }
+
+        private void btnShowFieldResult_Click(object sender, RoutedEventArgs e) {
+            if (richEditControl1.Document.Fields.Count > 0)
+                foreach (Field _f in richEditControl1.Document.Fields){
+                _f.ShowCodes = false;
+                }
+        }
+
+        private void btnModifyFieldCode_Click(object sender, RoutedEventArgs e) {
+            if (richEditControl1.Document.Fields.Count <= 0)
+                return;
+#region #coderange
+Document document = richEditControl1.Document;
+string fieldCode = document.GetText(document.Fields[0].CodeRange);
+if (!fieldCode.Contains("\\@")) {
+    DocumentPosition position = document.Fields[0].CodeRange.End;
+    document.InsertText(position, @"\@ ""MMMM""");
+    document.Fields[0].ShowCodes = true;
+}
+#endregion #coderange
+        }
+
+        private void btnCreateFieldFromSelection_Click(object sender, RoutedEventArgs e) {
+            Document document = richEditControl1.Document;
+            document.Fields.Add(document.Selection);
+        }
+    }
+}
